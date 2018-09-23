9ed71d9c2e6e6708b8e712a589c9bea00248ba24
Daniel Paras <dennyparas28@gmail.com>
2018-09-23 19:27:45 +0800

    add mongoose connect

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.Promise = Promise
+
+mongoose.connect(process.env.MONGODB_ACCOUNT)
+let db = mongoose.connection
+
+// Check connection
+db.once('open', () => {
+  console.log('Connected to Database')
+})
+
+// Check DB for errors
+db.on('error', (err) => {
+  console.log(err)
+})
 
 // Init app
-const app = express();
+const app = express()
 
 // Body Parser
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-// View Setup 
+// View setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-
 app.get('/', (req, res) => {
-    res.render('index')
+  res.render('index')
 })
 
 // Init server
 const port = 3000
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}`)
+  console.log(`Server running on port: ${port}`)
 })
