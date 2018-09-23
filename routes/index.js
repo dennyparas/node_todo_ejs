f6939910441604c0ba0baa517bd58eb02980c47f
Daniel Paras <dennyparas28@gmail.com>
2018-09-23 22:50:00 +0800

    add todo model  to render  todos from db

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (res, req) => {
-  res.render('index')
+const Todo = require('./../models/todo')
+
+// Index route
+router.get('/', async (req, res) => {
+  const todos = await Todo.find({})
+  res.render('index', { todos })
 })
+
+module.exports = router
