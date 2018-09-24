9e525c307a93939abbc294cbb34683fd46dad67a
Daniel Paras <dennyparas28@gmail.com>
2018-09-24 11:01:53 +0800

    add a delete id route

--- routes/todos.js
+++ routes/todos.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const Todo = require('./../models/todo')
+const ObjectID = require('mongodb').ObjectID
 
+// Post todo route
 router.post('/', async (req, res) => {
   const todo = new Todo({
     title: req.body.title,
@@ -11,6 +13,17 @@ router.post('/', async (req, res) => {
 
   await todo.save()
   res.redirect('/')
+})
+
+// Delete todo route
+router.delete('/:id', async (req, res) => {
+  const query = { _id: ObjectID(req.params.id) }
+  const todo = await Todo.findOneAndDelete(query)
+  if (!todo) {
+    return res.sendStatus(404).send('the todo with a given ID is not found')
+  }
+  console.log(`Todo ID ${query._id} is remove`)
+  res.sendStatus(200)
 })
 
 module.exports = router
