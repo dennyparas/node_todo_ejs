86efd53b10b3fcc5abbe997cb9fce7875f18b43a
Daniel Paras <dennyparas28@gmail.com>
2018-09-24 11:48:02 +0800

    add todo update and delete functionality

--- routes/todos.js
+++ routes/todos.js
@@ -4,17 +4,6 @@ const router = express.Router()
 const Todo = require('./../models/todo')
 const ObjectID = require('mongodb').ObjectID
 
-// Post todo route
-router.post('/', async (req, res) => {
-  const todo = new Todo({
-    title: req.body.title,
-    body: req.body.body
-  })
-
-  await todo.save()
-  res.redirect('/')
-})
-
 // Delete todo route
 router.delete('/:id', async (req, res) => {
   const query = { _id: ObjectID(req.params.id) }
@@ -24,6 +13,44 @@ router.delete('/:id', async (req, res) => {
   }
   console.log(`Todo ID ${query._id} is remove`)
   res.sendStatus(200)
+})
+
+// todo route by id
+router.get('/:id', async (req, res) => {
+  const query = { _id: ObjectID(req.params.id) }
+  const todo = await Todo.findById(query)
+  if (!todo) {
+    return res.sendStatus(404).send('the todo with a given ID is not found')
+  }
+  res.render('todo_single', { todo })
+})
+
+// todo update route
+router.post('/:id', async (req, res) => {
+  const query = { _id: ObjectID(req.params.id) }
+  const todo = await Todo.findByIdAndUpdate(query, {
+    $set: {
+      title: req.body.title,
+      body: req.body.body
+    }
+  },
+  { new: true }
+  )
+  if (!todo) {
+    return res.sendStatus(404).send('the todo with a given ID is not found')
+  }
+  res.redirect('/')
+})
+
+// Post todo route
+router.post('/', async (req, res) => {
+  const todo = new Todo({
+    title: req.body.title,
+    body: req.body.body
+  })
+
+  await todo.save()
+  res.redirect('/')
 })
 
 module.exports = router
